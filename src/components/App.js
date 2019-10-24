dee3ec6e7431d63eacaad2cf6aa8d7f62b8cb859
Kate <ekaterina.saplicza@mail.ru>
2019-10-24 16:17:24 +0300

    add home page component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles.css';
 import Navbar from './Navbar';
+import HomePage from './HomePage';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 
 class App extends React.Component {
@@ -14,7 +15,7 @@ class App extends React.Component {
           <Navbar/>
             <Router>
                 <Route exact path="/">
-                    {/*Home Page*/}
+                    <HomePage/>
                 </Route>
                 <Route strict path="/search">
                     {/*Search Page*/}
---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return(
+            <>
+
+            </>
+        )
+    }
+}
+
+export default HomePage;
