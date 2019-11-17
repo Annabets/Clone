fc728b0ccb3998f402533711d5d185951f837d7e
Kate <ekaterina.saplicza@mail.ru>
2019-11-18 01:11:37 +0300

    refactor photo service

--- src/services/photoService.js
+++ src/services/photoService.js
@@ -1,55 +1,39 @@
 import config from '../assets/config';
 
-function loadRandomPhoto() {
-    const requestOptions = {
+function getRequestOptions() {
+    return {
         method: 'GET',
-        headers: {'Authorization': config.API_key},
-    };
-    const page = Math.round(Math.random() * 1000);
+        headers: {'Authorization': config.API_key}
+    }
+}
 
-    return fetch(`https://api.pexels.com/v1/search?query=dark+wallpaper&per_page=1&page=${page}`, requestOptions)
-        .then(resp=>{
-            return resp.text().then(text =>{
-                if(!resp.ok)
-                    return Promise.reject(resp.statusText);
-                else
-                    return Promise.resolve(JSON.parse(text));
-            })
+function handleResponse(resp) {
+    return resp.text().then(text => {
+        if (!resp.ok)
+            return Promise.reject(resp.statusText);
+        else
+            return Promise.resolve(JSON.parse(text));
     })
 }
 
-function loadCuratedPhotos(page) {
-    const requestOptions = {
-        method: 'GET',
-        headers: {'Authorization': config.API_key},
-    };
+function loadRandomPhoto() {
+    const page = Math.round(Math.random() * 1000);
 
-    return fetch(`https://api.pexels.com/v1/curated?per_page=${config.per_page}&page=${page}`,requestOptions)
-        .then(resp => {
-            return resp.text().then(text => {
-                if(!resp.ok)
-                    return Promise.reject(resp.statusText);
-                else
-                    return Promise.resolve(JSON.parse(text));
-            })
-        })
+    return fetch(`${config.src}/search?query=dark+wallpaper&per_page=1&page=${page}`,
+        getRequestOptions())
+        .then(handleResponse)
+}
+
+function loadCuratedPhotos(page) {
+    return fetch(`${config.src}/curated?per_page=${config.per_page}&page=${page}`,
+        getRequestOptions())
+        .then(handleResponse)
 }
 
 function loadSearchPhotos(query, page) {
-    const requestOptions = {
-        method: 'GET',
-        headers: {'Authorization': config.API_key},
-    };
-
-    return fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=${config.per_page}&page=${page}`,requestOptions)
-        .then(resp => {
-            return resp.text().then(text => {
-                if(!resp.ok)
-                    return Promise.reject(resp.statusText);
-                else
-                    return Promise.resolve(JSON.parse(text));
-            })
-        })
+    return fetch(`${config.src}/search?query=${query}&per_page=${config.per_page}&page=${page}`,
+        getRequestOptions())
+        .then(handleResponse)
 }
 
 export const photoService = {
